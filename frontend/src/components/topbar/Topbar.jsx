c5acb03d4307e072821566d5581cecd032ec90b9
spadyala-melb <spadyala.melb@gmail.com>
2023-05-22 11:01:08 +1000

    navbar added at frontend

---
+++ frontend/src/components/topbar/Topbar.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  BsPersonFill,
+  BsChatLeftTextFill,
+  BsBellFill,
+  BsSearch,
+} from "react-icons/bs";
+import "./topbar.css";
+
+const Topbar = () => {
+  return (
+    <div>
+      <div className="topbar-container">
+        <div className="topbar-left">
+          <div className="topbar-logo">
+            <p>Padyala's Social Media</p>
+          </div>
+        </div>
+        <div className="topbar-center">
+          <div className="search-bar">
+            <span className="search-bar-icon">
+              <BsSearch />
+            </span>
+            <input
+              type="text"
+              placeholder="Search for person, photo or videos"
+            />
+          </div>
+        </div>
+        <div className="topbar-right">
+          <div className="topbar-links">
+            <div className="topbar-link">
+              <Link to="/">Homepage</Link>
+            </div>
+            <div className="topbar-link">
+              <Link to="/">Timeline</Link>
+            </div>
+          </div>
+          <div className="topbar-icons">
+            <div className="person">
+              <BsPersonFill className="person-icon" />
+              <span className="person-icon-notification-number">1</span>
+            </div>
+            <div className="chat">
+              <BsChatLeftTextFill className="chat-icon" />
+              <span className="chat-icon-notification-number">1</span>
+            </div>
+            <div className="bell">
+              <BsBellFill className="bell-icon" />
+              <span className="bell-icon-notification-number">1</span>
+            </div>
+          </div>
+          <div className="profile-pic">
+            <Link to="#">
+              <img src="./assets/person/1.jpeg" alt="" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+
+export default Home;
